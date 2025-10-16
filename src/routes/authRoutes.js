f7411e91e59bd23b4a957810c36df251220a0b4e
varunshine <varun.verma@shinedezign.com>
2025-10-16 17:42:17 +0530

    add validation for login mail.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { checkToken, login, refreshToken } from "../controllers/authController.js";
-import { checktokenValidation } from "../validations/authValidation.js";
+import { checktokenValidation, loginValidation } from "../validations/authValidation.js";
 import { validate } from "../middleware/validationMiddleware.js";
 
 const router = express.Router();
 
 router.post("/check-token",checktokenValidation,validate, checkToken);
 router.post("/refresh-token", refreshToken);
-router.post("/login", login);
+router.post("/login",loginValidation,validate, login);
 
 export default router;
--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -4,3 +4,7 @@ export const checktokenValidation = [
     body("token").notEmpty().withMessage("Token is required"),
     body("idToken").notEmpty().withMessage("Id Token is required"),
 ];
+
+export const loginValidation = [
+    body("mail").notEmpty().withMessage("Mail is required"),
+];
